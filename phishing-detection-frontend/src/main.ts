bf16a6b3a8e9df4d97f30fe31abdd8cbf185f1fe
Archit Mahule <archit.mahule@mathongo.com>
2025-02-14 15:17:59 +0530

    fix warnings

--- phishing-detection-frontend/src/main.ts
+++ phishing-detection-frontend/src/main.ts
@@ -3,13 +3,14 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
 import { routes } from './app/app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    importProvidersFrom(HttpClientModule), provideAnimationsAsync()  // Import HttpClientModule here
+    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withFetch())
   ]
 }).catch(err => console.error(err));
