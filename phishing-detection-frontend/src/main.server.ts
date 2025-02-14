e9c735e1dc8d30030767cd81966507802df4c80b
Archit Mahule <archit.mahule@mathongo.com>
2025-02-14 15:17:59 +0530

    fix warnings

--- phishing-detection-frontend/src/main.server.ts
+++ phishing-detection-frontend/src/main.server.ts
@@ -2,13 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 
 const bootstrap = () => bootstrapApplication(AppComponent, {
   ...config,
   providers: [
     ...(config.providers || []),
-    importProvidersFrom(HttpClientModule)  // Import HttpClientModule here
+    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withFetch())
   ]
 });
 
